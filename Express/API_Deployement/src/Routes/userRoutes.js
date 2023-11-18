69d96dbbd86312989a6a53f1fc78cf416e7af901
akashkhot12 <akashkhot03@gmail.com>
2023-11-18 15:41:52 +0530

    user control

--- Express/API_Deployement/src/Routes/userRoutes.js
+++ Express/API_Deployement/src/Routes/userRoutes.js
@@ -1,12 +1,9 @@
 const express = require("express");
+const { signup, signin } = require("../controllers/userController");
 const userRouter = express.Router();
 
-userRouter.post("/signup",(req,res)=>{
-res.send("signup")
-});
+userRouter.post("/signup",signup);
 
-userRouter.post("/singin",(req,res)=>{
-    res.send("signin")
-});
+userRouter.post("/singin",signin);
 
 module.exports = userRouter;
--- Express/API_Deployement/src/controllers/userController.js
+++ Express/API_Deployement/src/controllers/userController.js
@@ -1,9 +1,20 @@
 const userModels = require("../models/user");
 
-const signup = (req,res)=>{
+const signup = async(req,res)=>{
 
+    const{username,email,password}=req.body;
+    try {
+        const existingUser = await userModels.findOne({email:email})
+        if (existingUser) {
+            return res.status(400).json({message:"user already exists"})
+        }
+    } catch (error) {
+        
+    }
 }
 
-const signin = (req,res)=>{
+const signin = async(req,res)=>{
     
 }
+
+module.exports ={signup,signin}
