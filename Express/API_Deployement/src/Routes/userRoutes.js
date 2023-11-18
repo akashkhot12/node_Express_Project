b8087f2831063e398aaf8cc28304be854741fe43
akashkhot12 <akashkhot03@gmail.com>
2023-11-18 11:33:59 +0530

    exports router

--- Express/API_Deployement/src/Routes/userRoutes.js
+++ Express/API_Deployement/src/Routes/userRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const userRouter = express.Router();
 
 userRouter.post("/signup",(req,res)=>{
-
+res.send("signup")
 });
 
 userRouter.post("/singin",(req,res)=>{
-
+    res.send("signin")
 });
 
+module.exports = userRouter;
