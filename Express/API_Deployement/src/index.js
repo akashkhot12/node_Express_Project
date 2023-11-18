41bbe1e858412b53f3372e67002efafca13e1e4c
akashkhot12 <akashkhot03@gmail.com>
2023-11-18 11:18:04 +0530

    adding quotes

--- Express/API_Deployement/src/index.js
+++ Express/API_Deployement/src/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
-const app = express()
+const app = express();
+const quotes = require('./quotes.json')
 
 
 app.get('/',(req,res)=>{
     res.send("hello");
 })
+
+app.get('/quotes',(req,res)=>{
+    res.status(200).json(quotes)
+})
+
 app.listen(5000,()=>{
     console.log("server is started");
 })
