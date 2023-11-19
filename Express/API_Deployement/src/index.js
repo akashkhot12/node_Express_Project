c0857a703fc651e430f7d4a663143c40e0a12772
akashkhot12 <akashkhot03@gmail.com>
2023-11-19 11:48:35 +0530

    data insert successfully

--- Express/API_Deployement/src/index.js
+++ Express/API_Deployement/src/index.js
@@ -2,28 +2,24 @@ const express = require("express");
 const app = express();
 const userRouter = require("./Routes/userROutes");
 const noteRouter = require("./Routes/notesRoutes");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 app.use(express.json());
 
 app.use("/users", userRouter);
 app.use("/note", noteRouter);
 
-
-
-
 app.get("/", (req, res) => {
   res.send("hello");
 });
 
-mongoose.connect("mongodb+srv://akashkhot03:Akash3975@cluster0.ftplmdi.mongodb.net/?retryWrites=true&w=majority")
-.then(()=>{
+mongoose
+  .connect(
+    "mongodb+srv://akashkhot03:Akash3975@cluster0.ftplmdi.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
     app.listen(5000, () => {
-        console.log("server is started");
-      });
-})
-.catch((error)=>{
-
-})
-
-
+      console.log("server is started");
+    });
+  })
+  .catch((error) => {});
