6da7509315d39e729fd1212987135b7e595a3c06
akashkhot12 <akashkhot03@gmail.com>
2023-11-19 11:20:39 +0530

    sing in

--- Express/API_Deployement/src/controllers/userController.js
+++ Express/API_Deployement/src/controllers/userController.js
@@ -20,10 +20,12 @@ const signup = async(req,res)=>{
         });
 
         const token = jwt.sign({email:result.email,id:result._id},SECRET_KEY);
-        res.status(201).json
+        res.status(201).json({user:result,token:token});
+
 
     } catch (error) {
-        
+        console.log(error);
+        res.status(500).json({message:"something went wrong"});
     }
 }
 
