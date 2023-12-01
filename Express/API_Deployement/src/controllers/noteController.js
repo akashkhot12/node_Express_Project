ac0d3e4b438ce22a2dc864a9813a6f5873e00a20
akashkhot12 <akashkhot03@gmail.com>
2023-12-01 10:09:24 +0530

    create files

---
+++ Express/API_Deployement/src/controllers/noteController.js
@@ -0,0 +1,21 @@
+const nodeModule = require("../models/note")
+
+const createNote = (req,res)=>{
+
+}
+
+const updateNote = (req,res)=>{
+    
+}
+
+const deleteNote = (req,res)=>{
+    
+}
+
+const getNote = (req,res)=>{
+    
+}
+
+module.exports = {
+    createNote,updateNote,deleteNote,getNote
+}
