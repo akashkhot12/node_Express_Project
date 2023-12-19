8456333da8d996a533c33c21c72f08b4478e3b7e
akashkhot12 <akashkhot03@gmail.com>
2023-12-19 09:27:31 +0530

    database add

--- medAppoint/index.js
+++ medAppoint/index.js
@@ -1,6 +1,20 @@
 const express  = require('express');
 const app = express();
+const{MongoClient} = require('mongodb');
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const database = "ecom";
 
+const connections = async(req,res,next)=>{
+    try {
+        let result = await client.connect();
+    let db = result.db(database);
+    return db.collection("products")
+    next()
+    } catch (error) {
+        
+    }
+}
 
 app.listen(5000,()=>{
     console.log("server is up");
