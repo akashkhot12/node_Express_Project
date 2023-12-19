9ca82c140690104913887e4590e466e5bd18ea81
akashkhot12 <akashkhot03@gmail.com>
2023-12-19 10:02:30 +0530

    creating path for files

--- Login-Logout-System/index.js
+++ Login-Logout-System/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path  = require('path')
 const app = express();
 
 app.set("view engine",'ejs');
+
+// load static assets
+app.use('/static',express.static(path.join(__dirname,'public')))
 
 // home route
 app.get('/',(req,res)=>{
