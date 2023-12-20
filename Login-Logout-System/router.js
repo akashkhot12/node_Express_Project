2dfb5f3159fe153e40fe33bffb635198c5b9676f
akashkhot12 <akashkhot03@gmail.com>
2023-12-20 12:39:50 +0530

    document formated

--- Login-Logout-System/router.js
+++ Login-Logout-System/router.js
@@ -1,47 +1,49 @@
-var express = require('express');
-var router  = express.Router();
+var express = require("express");
+var router = express.Router();
 
 const credetials = {
-    email:"admin@gmail.com",
-    password:"admin@12345"
-}
+  email: "admin@gmail.com",
+  password: "admin@12345",
+};
 
 // login user
 
-router.post('/login',(req,res)=>{
-if (req.body.email=credetials.email&&req.body.password==credetials.password) {
-    req.session.user = req.body.email
-    res.redirect('/route/dashboard')
+router.post("/login", (req, res) => {
+  if (
+    (req.body.email =
+      credetials.email && req.body.password == credetials.password)
+  ) {
+    req.session.user = req.body.email;
+    res.redirect("/route/dashboard");
     // res.end("login successfull")
-}
-else{
-res.end("invalid username")
-}
-})
+  } else {
+    res.end("invalid username");
+  }
+});
 
 // routes for dashboards
 
-router.get('/dashboard',(req,res)=>{
-    if (req.session.user) {
-        res.render('dashboard',{user:req.session.user})
-    } else {
-        res.send("Unauthorize User")
-    }
-})
-
-
+router.get("/dashboard", (req, res) => {
+  if (req.session.user) {
+    res.render("dashboard", { user: req.session.user });
+  } else {
+    res.send("Unauthorize User");
+  }
+});
 
 // routes for loagout
-router.get('/logout',(req,res)=>{
-    req.session.destroy(function(err){
-        if (err) {
-            console.log(err);
-            res.send("Error")
-        }
-        else{
-           res.render('base',{title:"Express",logout:'logout Successfully..!'}) 
-        }
-    })
-})
+router.get("/logout", (req, res) => {
+  req.session.destroy(function (err) {
+    if (err) {
+      console.log(err);
+      res.send("Error");
+    } else {
+      res.render("base", {
+        title: "Express",
+        logout: "logout Successfully..!",
+      });
+    }
+  });
+});
 
 module.exports = router;
