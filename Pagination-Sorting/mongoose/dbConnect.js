15e2f9bbf51c40c00414ec51af98f68f9c7ad897
akashkhot12 <akashkhot03@gmail.com>
2024-01-30 10:13:49 +0530

    file changes

--- Pagination-Sorting/mongoose/dbConnect.js
+++ Pagination-Sorting/mongoose/dbConnect.js
@@ -1,21 +1,20 @@
 const mongoose = require("mongoose");
 
-const dbConnect =()=>{
-const connectionsParams = {useNewUrlParser:true};
-mongoose.connect(process.env.DB,connectionsParams);
+const dbConnect = () => {
+    const connectionParams = { useNewUrlParser: true };
+    mongoose.connect(process.env.DB, connectionParams);
 
-mongoose.connection.on("connected",()=>{
-    console.log("Connected to database successfully.");
-});
+    mongoose.connection.on("connected", () => {
+		console.log("Connected to database sucessfully");
+	});
 
-mongoose.connection.on("error",(err)=>{
-    console.log("Error while connecting to database"+err);
-});
+	mongoose.connection.on("error", (err) => {
+		console.log("Error while connecting to database :" + err);
+	});
 
-mongoose.connection.on("disconnected",()=>{
-    console.log("Mongodb connection disconnected.");
-})
-
+	mongoose.connection.on("disconnected", () => {
+		console.log("Mongodb connection disconnected");
+	});
 };
 
 module.exports = dbConnect;
--- Pagination-Sorting/mongoose/server.js
+++ Pagination-Sorting/mongoose/server.js
@@ -1,21 +1,16 @@
 require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
 const dbConnect = require("./dbConnect");
-const movieRoutes = require("./routes/movies")
+const movieRoutes = require("./routes/movies");
+const cors = require("cors");
 const app = express();
 
 dbConnect();
 
 app.use(express.json());
 app.use(cors());
-app.use("/api",movieRoutes)
 
+app.use("/api", movieRoutes);
 
-
-
-const port = process.env.PORT||8080;
-
-app.listen(port,()=>{
-    console.log(`listening on port ${port}...`);
-})
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
