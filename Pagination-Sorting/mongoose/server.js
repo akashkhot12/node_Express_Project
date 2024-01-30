920e62bbd509d6e7df6af1b0484d93c95c3f26d4
akashkhot12 <akashkhot03@gmail.com>
2024-01-30 09:40:22 +0530

    create a model

--- Pagination-Sorting/mongoose/server.js
+++ Pagination-Sorting/mongoose/server.js
@@ -1,10 +1,12 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const dbConnect = require("./dbConnect");
 const app = express();
 
-app.use(express.json());
+dbConnect();
 
+app.use(express.json());
 app.use(cors());
 
 
