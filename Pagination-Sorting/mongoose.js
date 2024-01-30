37e99c2144cb5c0589c5ee3647f0e7c26663a4d8
akashkhot12 <akashkhot03@gmail.com>
2024-01-30 09:26:58 +0530

    create server for pagingations

---
+++ Pagination-Sorting/mongoose.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+
+const port = process.env.PORT||8080;
+
+app.listen(port,()=>{
+    console.log(`listening on port ${port}...`);
+})
