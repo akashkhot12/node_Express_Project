301917bc917a079af7e4ba2c5026539fb6e4526a
akashkhot12 <akashkhot03@gmail.com>
2024-02-10 12:57:54 +0530

    complete login signup project

--- signUp-Login/src/index.js
+++ signUp-Login/src/index.js
@@ -30,23 +30,53 @@ app.get("/signup", (req, res) => {
 app.post('/signup', async (req, res) => {
     const data = {
         name: req.body.username,
-        email:req.body.email,
+        email: req.body.email,
         password: req.body.password
     }
 
     // checked user already exist in databse 
-    const existingUser = await collection.findOne({ name: data.name })
+    const existingUser = await collection.findOne({ email: data.name })
 
     if (existingUser) {
         res.send("user already exists");
     }
     else {
+
+        // hashed password use methode 
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+        data.password = hashedPassword //replace the hashed pasword with original password 
+
+
         const userdata = await collection.insertMany(data);
         console.log(userdata);
     }
-
-
 })
+
+// login user 
+ app.post("/login",async(req,res)=>{
+    try{
+        const check = await collection.findOne({email:req.body.email});
+        if (!check) {
+            res.send("this user cannot found")
+        }
+
+        // compare the hash password from the database with the pain text .
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password,check.password)
+        if (isPasswordMatch) {
+            res.render("home")
+        }
+        else{
+             res.send("wrong password")
+        }
+    }
+    catch{
+        res.send("wrong Credentials");
+    }
+ })
+
 
 
 app.listen(5000, () => {
