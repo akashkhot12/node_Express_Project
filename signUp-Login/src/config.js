769b3e23ec20ee7579df53662140415b6523fa5b
akashkhot12 <akashkhot03@gmail.com>
2024-02-10 12:25:22 +0530

    adding style

--- signUp-Login/src/config.js
+++ signUp-Login/src/config.js
@@ -14,6 +14,10 @@ connect.then(()=>{
 const loginSchema  = new mongoose.Schema({
     name:{
         type:String,
+        require:false
+    },
+    email:{
+        type:String,
         require:true
     },
    password:{
@@ -21,3 +25,9 @@ const loginSchema  = new mongoose.Schema({
     require:true
    }
 });
+
+// create a model (collection part )
+
+const collection = new mongoose.model("users",loginSchema);
+
+module.exports = collection;
--- signUp-Login/src/index.js
+++ signUp-Login/src/index.js
@@ -1,8 +1,14 @@
-const express  = require("express");
+const express = require("express");
 const path = require("path");
 const bcrypt = require('bcrypt');
-
+const collection = require("./config");
+const { log } = require("console");
 const app = express();
+
+// convert data into the json 
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }))
 
 
 // Set EJS as the view engine
@@ -11,15 +17,38 @@ app.set('view engine', 'ejs');
 // static file
 app.use(express.static("public"))
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render('login')
 })
 
-app.get("/signup",(req,res)=>{
+app.get("/signup", (req, res) => {
     res.render('signup');
 })
 
 
-app.listen(5000,()=>{
+// register methode
+app.post('/signup', async (req, res) => {
+    const data = {
+        name: req.body.username,
+        email:req.body.email,
+        password: req.body.password
+    }
+
+    // checked user already exist in databse 
+    const existingUser = await collection.findOne({ name: data.name })
+
+    if (existingUser) {
+        res.send("user already exists");
+    }
+    else {
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+    }
+
+
+})
+
+
+app.listen(5000, () => {
     console.log("server running on port 5000");
 })
