5731ac7bc445db4239436853c0d4082753c85d5d
akashkhot12 <akashkhot03@gmail.com>
2024-02-15 14:41:21 +0530

    accessed succefully

--- JWT-Token/index.js
+++ JWT-Token/index.js
@@ -1,56 +1,56 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 const app = express();
-const secreteKey = 'secreteKey'
+const secreteKey = "secreteKey";
 
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
+  res.json({
+    message: "a simple api",
+  });
+});
+
+app.post("/login", (req, res) => {
+  const user = {
+    id: 1,
+    username: "anil",
+    email: "anil@gmail.com",
+  };
+  jwt.sign({ user }, secreteKey, { expiresIn: "300s" }, (err, token) => {
     res.json({
-        message:"a simple api"
-    })
-})
+      token,
+    });
+  });
+});
 
-app.post('/login',(req,res)=>{
-    const user={
-        id:1,
-        username:"anil",
-        email:"anil@gmail.com"
-    }
-    jwt.sign({user},secreteKey,{expiresIn:'300s'},(err,token)=>{
-       res.json({
-        token
-       })
-    })
-})
-
-app.post('/profile',verifyingToken,(req,res)=>{
-jwt.verify(req.token,secreteKey,(err,authdata)=>{
+app.post("/profile", verifyingToken, (req, res) => {
+  jwt.verify(req.token, secreteKey, (err, authdata) => {
     if (err) {
-        res.send({
-            result:"invalid token"
-        })
+      res.send({
+        result: "invalid token",
+      });
     } else {
-        res.json({
-            message:"profile accessed",authdata
-        })
+      res.json({
+        message: "profile accessed",
+        authdata,
+      });
     }
-})
-})
+  });
+});
 
-
-function verifyingToken(req,res,next){
-const bearerHeader=req.headers['authorization'];
-if (typeof bearerHeader !==undefined ) {
+function verifyingToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== undefined) {
     const bearer = bearerHeader.split(" ");
-    const token=bearer[1];
+    const token = bearer[1];
     req.token = token;
-    next()
-} else {
+    next();
+  } else {
     res.send({
-        result:"token is not valid"
-    })
-}
+      result: "token is not valid",
+    });
+  }
 }
 
-app.listen(8000,()=>{
-    console.log("sserver is start");
-})
+app.listen(8000, () => {
+  console.log("sserver is start");
+});
