8d7dad5feb5f1e73b8885bdf199635d71ccaeb0b
akashkhot12 <akashkhot03@gmail.com>
2024-02-15 09:56:05 +0530

    create connections

---
+++ Crud/connect.js
@@ -0,0 +1,12 @@
+const{MongoClient}= require("mongodb");
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+const database = 'crud';
+
+
+const connect = async()=>{
+    let result = await client.connect();
+    let db = await result.db(database);
+    return db.collection("collections");
+}
+connect()
