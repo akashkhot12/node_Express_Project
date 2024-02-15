053a618f7379fab13d11d79f782d2458c020ec5d
akashkhot12 <akashkhot03@gmail.com>
2024-02-15 10:41:31 +0530

    creating  status code for postman

--- API-CRUD/Api.js
+++ API-CRUD/Api.js
@@ -1,2 +1,31 @@
 const mongodb = require('./connections');
-const express = require('express')
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.get('/',async(req,res)=>{
+    let data = await mongodb();
+    let readData = await data.find().toArray();
+    console.log(readData);
+    res.send(readData);
+})
+
+app.post('/add',async(req,res)=>{
+    let data = await mongodb();
+    let addData = await data.insertOne({
+        name:"akash",
+        age:32,
+    gender:"male"  
+  })
+  if (addData.acknowledged) {
+    res.send("data isernt succefully")
+  }
+  else{
+    res.statusCode(404)
+  }
+})
+
+app.listen(5500,()=>{
+    console.log("server is running");
+})
