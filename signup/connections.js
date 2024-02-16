452332f313d4e4ef0a4a1a96d37cf9e9877fcc4d
akashkhot12 <akashkhot03@gmail.com>
2024-02-16 11:58:07 +0530

    adding data

--- signup/connections.js
+++ signup/connections.js
@@ -3,11 +3,11 @@ const url = 'mongodb://localhost:27017'
 const client = new MongoClient(url);
 const database  = 'SignUp';
 
-const fectConnections  = async()=>{
+const fechConnections  = async()=>{
     let result = await client.connect();
     let db = await result.db(database);
     return db.collection('Details')
 
 }
 
-module.exports = fectConnections;
+module.exports = fechConnections;
--- signup/index.js
+++ signup/index.js
@@ -1,39 +1,13 @@
 const express = require('express');
-const connect = require('./connections')
 const app = express();
-const User = require('./User');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const linkdb = require('./connections');
+
 
 app.use(express.json());
 
-app.get('/registers',async(req,res)=>{
-    let db = await connect();
-    let showDeatails = await db.findOne();
-    res.send(showDeatails);
-})
 
+// get details 
 
-app.post('/signup',async(req,res)=>{
-    try {
-        const {username,email,password} = req.body;
-        // cheack if existing user or not 
-        const existingUser = await User.findOne({email});
-        if (existingUser) {
-            return res.status(400).send({ error: 'Email is already registered' });
-        }
-
-        // create a new user 
-        const newUser = new User({sername,email,password});
-        await newUser.save();
-        res.status(201).json({ message: 'User created successfully' });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Server error' });
-    }
-})
-
-app.listen(5000,()=>{
-    console.log("server is up on 5000 port ");
+app.get('/',async(req,res)=>{
+    
 })
