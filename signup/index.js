5cfda7e1af8e555addb4e8bb0c4e7a00a339da54
akashkhot12 <akashkhot03@gmail.com>
2024-02-16 12:50:47 +0530

    adding changes

--- signup/index.js
+++ signup/index.js
@@ -11,42 +11,42 @@ app.use(express.json());
 
 // get details 
 
-app.get('/',async(req,res)=>{
+app.get('/', async (req, res) => {
     let result = await linkdb();
     let db = await result.find().toArray();
     res.send(db)
 })
 
-app.post('/addDetails',async(req,res)=>{
+app.post('/addDetails', async (req, res) => {
     let result = await linkdb();
     const { username, email, password } = req.body;
-        try {
-            // email is required
-            if (!req.body.email ) {
-                return res.status(400).json({ error: 'Email is required' });
-            }else if(!req.body.username){
-                return res.status(400).json({ error: 'username is required' });
-            }else if(!req.body.password){
-                return res.status(400).json({ error: 'password is required' });
-            }
-            // Check if user with the provided email already exists
-            const existingUser = await result.findOne({ email });
-            if (existingUser) {
-              return res.status(400).json({ error: 'User already exists' });
-            }
-            
-    
-            // Insert the new user
-            await result.insertOne({ username, email, password });
-    
-            res.status(201).json({ message: 'User created successfully' });
-          } catch (error) {
-            console.error(error);
-            res.status(500).json({ error: 'Server error' });
-          }
-  });
-    
+    try {
+        // email,password,username is required
+        if (!req.body.email) {
+            return res.status(400).json({ error: 'Email is required' });
+        } else if (!req.body.username) {
+            return res.status(400).json({ error: 'username is required' });
+        } else if (!req.body.password) {
+            return res.status(400).json({ error: 'password is required' });
+        }
+        // Check if user with the provided email already exists
+        const existingUser = await result.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
 
-app.listen(3000,()=>{
+
+        // Insert the new user
+        await result.insertOne({ username, email, password });
+
+        res.status(201).json({ message: 'User created successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+
+app.listen(3000, () => {
     console.log("server is up on port number 3000");
 })
