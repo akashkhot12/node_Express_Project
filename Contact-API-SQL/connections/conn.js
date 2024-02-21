899a67bc027e17712bd88b0793140dc49830ecaf
akashkhot12 <akashkhot03@gmail.com>
2024-02-21 12:09:51 +0530

    connected to the database

--- Contact-API-SQL/connections/conn.js
+++ Contact-API-SQL/connections/conn.js
@@ -1,8 +1,7 @@
-const Connections = {
+const Connections = new Pool({
     host: "localhost",
     user: "root",
     password: "Akash3975",
     database: "Contacts"
-  }
-  
+  })
   module.exports = Connections;
---
+++ Contact-API-SQL/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const {Pool} = require('pg');
+
+
+const pool = new Pool({
+    host: "localhost",
+    user: "postgres",
+    password: "Akash3975",
+    database: "Contacts"
+});
+
+// Test PostgreSQL connection
+pool.connect((err, client, done) => {
+    if (err) {
+        console.error('Error connecting to PostgreSQL:', err);
+    } else {
+        console.log('Connected to PostgreSQL');
+        done(); // Release the client back to the pool
+    }
+});
+
+app.get('/',async(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(5000,()=>{
+    console.log("server is up");
+})
