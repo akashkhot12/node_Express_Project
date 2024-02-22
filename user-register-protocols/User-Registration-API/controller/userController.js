5f48289eeac8bc614f3c38a2cec22395e7374d8c
akashkhot12 <akashkhot03@gmail.com>
2024-02-22 11:51:06 +0530

    create controller

--- user-register-protocols/User-Registration-API/controller/userController.js
+++ user-register-protocols/User-Registration-API/controller/userController.js
@@ -3,4 +3,12 @@ const mongoose = rrequire('mongoose');
 const userSchema = require('../model/userModel')
 const app = express();
 
+const signUp = async(req,res)=>{
+res.send("hello this is akash")
+}
 
+const signIp = async(req,res)=>{
+    res.send("hello this is khot")
+}
+
+module.exports = {signIp,signUp};
