430732d2c23c475f02e739d79c2caa1bb6a0a964
akashkhot12 <akashkhot03@gmail.com>
2024-02-22 11:10:31 +0530

    create mongoose database

---
+++ user-register-protocols/User-Registration-API/server.js
@@ -0,0 +1,9 @@
+const express = require('mongodb')
+const mongoose = require('mongoose');
+const app = express();
+
+
+mongoose.connect("mongodb+srv://akashkhot03:Akash3975@cluster0.89dnsic.mongodb.net")
+.then(){
+    
+}
--- user-register-protocols/User-Registration-API/server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const mongodb = require('../connections/conn');
-
-app.use(mongodb);
-app.listen(5000,()=>{
-    console.log("server is up on");
-})
