910f39655b2f73964f290922cc7160b0fc7f65e0
akashkhot12 <akashkhot03@gmail.com>
2024-02-23 11:33:21 +0530

    add schema

---
+++ S&L&LResetPassword/model.js
@@ -0,0 +1,19 @@
+const { Timestamp } = require('mongodb');
+const mongoose = require('mongoose');
+
+const userModel = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        require:true
+    }
+},{timestamps:true});
+
+module.exports = userModel;
