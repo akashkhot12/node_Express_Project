a08462aabdf3b5d56fc9aeb651d473162f554b76
akashkhot12 <akashkhot03@gmail.com>
2024-03-01 11:07:57 +0530

    user model

---
+++ User-And-Contact-API/controller/api.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const model = require('../model/schema')
+
+
+const register = async(req,res)=>{
+    const{firstName,lastName,userName,Password,email,phone} 
+}
+
+// user Exist or not 
+    // create a hashed password
+    // create a signUp
+    // generate token 
