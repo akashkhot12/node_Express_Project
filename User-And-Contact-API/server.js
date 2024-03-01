783dcc8b3ff5a5ff054afaecf22ab4824a4022c0
akashkhot12 <akashkhot03@gmail.com>
2024-03-01 10:56:24 +0530

    create router

--- User-And-Contact-API/server.js
+++ User-And-Contact-API/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const router = require('./router/userRouter');
 
+
+app.use(express.json());
+app.use('/user',router);
 
 
 mongoose.connect('mongodb+srv://akashkhot03:Akash3975@cluster0.iygfbux.mongodb.net/')
