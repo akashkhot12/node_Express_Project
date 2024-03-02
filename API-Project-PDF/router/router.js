cd3a2f5407ff55e582be7c2efb970421eed8cf3a
akashkhot12 <akashkhot03@gmail.com>
2024-03-02 15:21:00 +0530

    adding forgot and reset password

--- API-Project-PDF/router/router.js
+++ API-Project-PDF/router/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getAllUsers, getByID } = require('../controller.js/api');
+const { register, login, getAllUsers, getByID, forgotPassword, resetPassword } = require('../controller.js/api');
  const router = express.Router()
 
 
@@ -7,6 +7,8 @@ router.post('/register',register);
 router.post('/login',login);
 router.get('/getUsers',getAllUsers);
 router.get('/users/:id', getByID);
+router.post('/reset', resetPassword);
+router.post('/forgot', forgotPassword);
 
 
 module.exports  = router;
