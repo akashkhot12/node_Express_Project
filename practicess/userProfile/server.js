9a05342a2e57b0f6ecb9992042370dc1b127e553
akashkhot12 <akashkhot03@gmail.com>
2024-03-05 11:18:41 +0530

    c onnection successfull

--- practicess/userProfile/server.js
+++ practicess/userProfile/server.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
 
+mongoose.connect("mongodb+srv://akashkhot03:Akash3975@api.rloabth.mongodb.net/")
+.then(()=>{
+    app.listen(5500,()=>{
+        console.log("app is running on port 5500");
+    })
+}).catch((error)=>{
+    console.log(error);
+}) 
