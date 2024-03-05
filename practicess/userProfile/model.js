8afa43ab51406b7aa54f6f9fca8f71735fdd8be6
akashkhot12 <akashkhot03@gmail.com>
2024-03-05 11:25:15 +0530

    adding insert data

---
+++ practicess/userProfile/model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+
+const user = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true
+    },
+    email:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+})
+
+
+exports.module = mongoose.model('user',user);
---
+++ practicess/userProfile/router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const model = require('./model')
+
+
+router.post('/insert',async(req,res)=>{
+    const {username,email,password} = req.body;
+    try {
+        let insertData = 
+    } catch (error) {
+        
+    }
+})
