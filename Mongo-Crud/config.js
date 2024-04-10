88159abdf722c00ededbbc34ca50635bbaad922d
akashkhot12 <akashkhot03@gmail.com>
2024-04-10 10:17:16 +0530

    create a connections file

---
+++ Mongo-Crud/config.js
@@ -0,0 +1,12 @@
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://localhost:27017';
+const client = MongoClient(url);
+const database  = 'Mongo-Crud';
+
+const connections = async()=>{
+    let result = await client.connect();
+    let db  = await result.db(database);
+    return db.collection('Crud_Data'); 
+}
+
+module.exports = connections;
