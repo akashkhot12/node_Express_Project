1215bc1aabf8ffd7b009fe8ed735e167f3c10c08
akashkhot12 <akashkhot03@gmail.com>
2024-06-21 22:45:19 +0530

    adding changes

--- mysql-crud/server.js
+++ mysql-crud/server.js
@@ -1,16 +1,34 @@
-const express  = require('express');
-const app = express();
-const port  = 8000;
+const mysql = require('mysql');
 
-// middleware
+const connection = mysql.createConnection({
+  host: 'localhost',      // Replace with your MySQL host
+  user: 'root',           // Replace with your MySQL username
+  password: 'Akash3975',   // Replace with your MySQL password
+  database: 'node-mysql' // Replace with your MySQL database name
+});
 
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + connection.threadId);
+});
 
-// routes
-app.get('/test',(req,res)=>{
-    res.send("hello")
-})
+// Example query
+connection.query('SELECT * FROM table_name', (error, results, fields) => {
+  if (error) {
+    console.error('Error executing query:', error.stack);
+    return;
+  }
+  console.log('Results:', results);
+});
 
-// listen
-app.listen(port,()=>{
-    console.log("server is started.");
-})
+// Close the connection
+connection.end((err) => {
+  if (err) {
+    console.error('Error ending the connection:', err.stack);
+    return;
+  }
+  console.log('Connection closed');
+});
