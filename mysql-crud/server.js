4eb07c7c27142d2ae659f6787d714b8ad079fd93
akashkhot12 <akashkhot03@gmail.com>
2024-06-22 11:23:23 +0530

    adding env

--- mysql-crud/server.js
+++ mysql-crud/server.js
@@ -1,34 +1,29 @@
+const express = require('express');
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',      // Replace with your MySQL host
-  user: 'root',           // Replace with your MySQL username
-  password: 'Akash3975',   // Replace with your MySQL password
-  database: 'node-mysql' // Replace with your MySQL database name
+
+const app = express();
+
+const db = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'',
+  database:'node-mysql'
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to the database:', err.stack);
-    return;
+db.connect((err)=>{
+  if(err){
+    console.log(err);
+  }else{
+    console.log("database connected");
   }
-  console.log('Connected to the database as id ' + connection.threadId);
 });
 
-// Example query
-connection.query('SELECT * FROM table_name', (error, results, fields) => {
-  if (error) {
-    console.error('Error executing query:', error.stack);
-    return;
-  }
-  console.log('Results:', results);
-});
 
-// Close the connection
-connection.end((err) => {
-  if (err) {
-    console.error('Error ending the connection:', err.stack);
-    return;
-  }
-  console.log('Connection closed');
-});
+app.get('/',(req,res)=>{
+  res.send('hello world')
+})
+
+app.listen(8000,()=>{
+  console.log("server is started.");
+})
