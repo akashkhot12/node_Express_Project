44f48294d7a91d77f3ac73797aaa63f1a54bc87d
akashkhot12 <akashkhot03@gmail.com>
2024-06-24 16:35:21 +0530

    adding crud

--- crud-mysql/server.js
+++ crud-mysql/server.js
@@ -16,8 +16,28 @@ app.get('/',(req,res)=>{
 
 app.post('/',(req,res)=>{
     const data = req.body;
-
     conn.query('insert into users set ?',data,(err,result,fields)=>{
+        if (err) {
+            throw err;
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+app.put('/:id',(req,res)=>{
+    const data =[req.body.username,req.body.email,req.params.id];
+    conn.query("update users set username = ? , email = ? where id = ?",data,(err,result)=>{
+        if (err) {
+            throw err;
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+app.delete('/:id',(req,res)=>{
+    conn.query("delete from users where id ="+ req.params.id,(err,result)=>{
         if (err) {
             throw err;
         } else {
@@ -29,3 +49,5 @@ app.post('/',(req,res)=>{
 app.listen(5000,()=>{
     console.log("server on.");
 })
+
+
