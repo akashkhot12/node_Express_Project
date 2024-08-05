77ffdd854d8020daa5f3447b924515b8fda5029f
akashkhot12 <akashkhot03@gmail.com>
2024-08-05 09:46:11 +0530

    importing routes

--- sequalized/youtube-english/index.js
+++ sequalized/youtube-english/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const apiRoutes = require("./routes");
 const PORT = 3000;
+
+app.use("/api", apiRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "hello world" });
--- sequalized/youtube-english/routes.js
+++ sequalized/youtube-english/routes.js
@@ -1,24 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/todos", (req, res) => {});
 
+router.post("/todos", (req, res) => {});
 
-router.get('/todos',(req,res)=>{
+router.get("/todos/:id", (req, res) => {});
 
-});
+router.put("/todos/:id", (req, res) => {});
 
-router.post('/todos',(req,res)=>{
+router.delete("/todos/:id", (req, res) => {});
 
-});
-
-router.get('/todos/:id',(req,res)=>{
-
-});
-
-router.put('/todos/:id',(req,res)=>{
-
-});
-
-router.delete('/todos/:id',(req,res)=>{
-
-});
+module.exports = router;
