46a361ae485310d7837f26389bf261349cab5d12
akashkhot12 <akashkhot03@gmail.com>
2024-08-05 09:44:09 +0530

    adding routes

---
+++ sequalized/youtube-english/routes.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+router.get('/todos',(req,res)=>{
+
+});
+
+router.post('/todos',(req,res)=>{
+
+});
+
+router.get('/todos/:id',(req,res)=>{
+
+});
+
+router.put('/todos/:id',(req,res)=>{
+
+});
+
+router.delete('/todos/:id',(req,res)=>{
+
+});
